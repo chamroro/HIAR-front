d1b20d6c9ce1ee8a314041f06ebca31c5f52ff9d
chamroro <chamsae@handong.ac.kr>
2022-08-25 15:41:22 +0900

    heroku test

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -45,7 +45,6 @@
           displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
         }
       } else {
-     
         php_email_form_submit(thisForm, action, formData);
       }
     });
