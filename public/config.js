1b4e843a248144bcef97d05b8106b2b31f73d75f
chamroro <chamsae@handong.ac.kr>
2022-09-12 17:41:13 +0900

    front

---
+++ public/config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFqqXQTcWYPpOATD9HTNdjw9VBlWxSyKU",
+    authDomain: "hiar-74d1e.firebaseapp.com",
+    projectId: "hiar-74d1e",
+    storageBucket: "hiar-74d1e.appspot.com",
+    messagingSenderId: "404361333109",
+    appId: "1:404361333109:web:4857ac941333f45c126b3c",
+    measurementId: "G-ZFQF3RD0XZ"
+  };
+  
+firebase.initializeApp(firebaseConfig);
